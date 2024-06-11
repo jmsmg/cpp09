cbb56ac249599f644032451b4be97c9d91ceab16
jmsmg <jmsmg1@me.com>
2024-06-11 07:00:02 +0000

    input argu ok

--- ex02/PmergeMe.hpp
+++ ex02/PmergeMe.hpp
@@ -7,8 +7,10 @@
 #include <cstdlib>
 #include <climits>
 #include <deque>
+#include <set>
 #include <utility>
 #include <cmath>
+#include <algorithm>
 
 template <typename T>
 class PmergeMe
@@ -18,21 +20,100 @@ class PmergeMe
 		T					_array;
 
 	public:
-		PmergeMe();
-		PmergeMe(size_t n);
-		PmergeMe(const PmergeMe &pmergeme);
-		PmergeMe	&operator=(const PmergeMe &pmergeme);
-		~PmergeMe();
+		PmergeMe() : _n(0), _array()
+		{
+		};
+		
+		PmergeMe(size_t n) : _n(n), _array()
+		{
+		};
+		
+		PmergeMe(const PmergeMe &pmergeme)
+		{
+			*this = pmergeme;
+		};
+		PmergeMe	&operator=(const PmergeMe &pmergeme)
+		{
+			if (this != &pmergeme)
+			{
+				this->_n = pmergeme._n;
+				this->_array = pmergeme._array;
+			}
+			return (*this);
+		};
+		~PmergeMe()
+		{
+		};
 
-		void	inputArguments(int argc, char *argv[]);
-		void	fordJohnson(T &array, int depth, int fair_size);
-		void	mergeArray(T &array, int fair_size);
-		int		jacobsthal(int n);
+		void	inputArguments(int argc, char *argv[])
+		{
+			std::set<int>		tmp;
+			std::vector<int>	vec;
+			if (argc < 2)
+			{
+				throw (std::invalid_argument("Error : invalid argument count"));
+			}
+			for (int i = 1; i < argc; i++)
+			{
+				for (int j = 0; argv[i][j]; j++)
+				{
+					if (!std::isdigit(argv[i][j]))
+					{
+						std::cerr << static_cast<std::string>(argv[i]) << std::endl;
+						throw (std::invalid_argument("Error : is not a natural number"));
+					}
+					else if (INT_MAX < atoll(argv[i]))
+					{
+						std::cout << argv[i] << std::endl;
+						throw (std::runtime_error("Error : argument is not intger"));
+					}
+				}
+				this->_array.push_back(atoi(argv[i]));
+				tmp.insert(atoi(argv[i]));
+				if (tmp.size() != this->_array.size())
+					throw (std::runtime_error("Error : duplicate number"));
+			}
+		};
 
-		size_t				getN();
-		T					&getArray();
+		void	fordJohnson(T &array, int depth, int fair_size)
+		{
+			if (array.size() == 0 || array.size() == 1) // 에러처리
+			{
+				throw (std::runtime_error("Error : invalid array size"));
+			}
 
-		void				setN(size_t n);
-		void				setArray(T array);
+			size_t idx = 0;
+			while (fair_size * (idx + 1) < this->_n)
+			{
+				typename T::iterator a = array.begin() + idx * fair_size;
+				typename T::iterator b = a + (fair_size / 2);
+				typename T::iterator last = b + (fair_size / 2);
+				if (*a < *b)
+					std::rotate(a, b, last);
+				idx++;
+			}
+
+			if (idx == 1) // 탈출
+			{
+				this->_array = array;
+				return ;
+			}
+
+			fordJohnson(array, depth + 1, fair_size * 2);
+			// this->mergeArray();
+			// 이후에 각 depth 3, 2, 1로 올라가며 정렬 진행?
+			// 값으로 찾기
+		};
+		// void	mergeArray(T &array, int fair_size);
+		int		jacobsthal(int n)
+		{
+			return ((pow(2, n) - pow(-1, n)) / 3);
+		};
+
+		// size_t				getN();
+		// T					&getArray();
+
+		// void				setN(size_t n);
+		// void				setArray(T array);
 };
 #endif
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -4,8 +4,14 @@ int	main(int argc, char *argv[])
 {
 	try
 	{
-		std::cout << argc << std::endl;
-		std::cout << argv[1] << std::endl;
+		// std::cout << argc << std::endl;
+		// std::cout << argv[1] << std::endl;
+		std::vector<int> array;
+
+		PmergeMe<std::vector<int> > a;
+
+		a.inputArguments(argc, argv);
+		// a.fordJohnson(array, 1, 2);
 	}
 	catch(const std::exception& e)
 	{
