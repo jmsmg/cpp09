84b8affa854d176e817da2db6f84a8f8223f0cf6
jmsmg <jmsmg1@me.com>
2024-06-14 05:46:45 +0900

    sort 비교

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -13,6 +13,11 @@ int	main(int argc, char *argv[])
 			std::cout << a.getArray()[i] << " ";
 		}
 		std::cout << std::endl;
+		std::sort(a.getArray().begin(), a.getArray().end());
+		for (size_t i = 0; i < a.getArray().size(); i++)
+		{
+			std::cout << a.getArray()[i] << " ";
+		}
 	}
 	catch(const std::exception& e)
 	{
